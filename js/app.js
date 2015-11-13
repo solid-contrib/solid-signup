5b27a2f5f8e5c061f97dab03e51c6cf41c54b574
deiu <andrei@fcns.eu>
2015-11-13 17:44:10 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+(function() {
+  var cards = document.querySelectorAll(".card.effect__click");
+  for ( var i  = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener( card );
+  }
+
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
+})();
+
+var setProgression = function(val) {
+  if (val) {
+    document.querySelector(".progression").style.width = val;
+  }
+};
+
+
